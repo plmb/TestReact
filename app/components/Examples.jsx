a9b4411929ada9131ad3b9cfc680901caed8fdec
Timur Khatypov <t.khatypov@gmail.com>
2016-12-27 11:27:53 +0300

    Style example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return(
       <div>
-        <h3>Welcome to examples page!</h3>
+        <h1 className="text-center">Examples!</h1>
+        <p>Here are a few example location to try out!</p>
+        <ol>
+          <li>
+            <Link to="/?location=Moscow">Moscow, Russia</Link>
+          </li>
+          <li>
+            <Link to="/?location=Rio">Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
     );
 }

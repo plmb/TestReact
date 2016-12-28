968a9b29d005793b43bb1678d612a192d6bdbbc2
Timur Khatypov <t.khatypov@gmail.com>
2016-12-28 16:55:20 +0300

    Style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
     return(
-      <h3>About component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>This is about page.</p>
+      </div>
     );
 }
 
